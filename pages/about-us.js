a9b0d3c28a69a8fb60067c36ad3cf91cbe1f26d1
Shahnas-P <shahanasp9@gmail.com>
2024-07-05 16:35:41 +0530

    feat(): Add about and contact pages

---
+++ pages/about-us.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <div>
+      <Head>
+        <title>About Page</title>
+        <meta name="description" content="This is our About page" />
+      </Head>
+
+      <main>
+        <h1>About Us</h1>
+      </main>
+    </div>
+  );
+}
---
+++ pages/contact-us.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+
+export default function Contact() {
+  return (
+    <div>
+      <Head>
+        <title>Contact Page</title>
+        <meta name="description" content="This is our Contact Page" />
+      </Head>
+
+      <main>
+        <h1>Contact Us</h1>
+      </main>
+    </div>
+  );
+}
